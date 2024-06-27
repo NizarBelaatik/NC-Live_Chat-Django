d588fdacfedb033601c91c1348031604c5f45231
The-Night-Code <43081584+The-Night-Code@users.noreply.github.com>
2024-06-28 00:18:58 +0100

    v0.0.4

--- LiveChat_app/templatetags/costum_tags.py
+++ LiveChat_app/templatetags/costum_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.timesince import timesince
+from django.utils import timezone
+from datetime import datetime
 
 register = template.Library()
 
@@ -8,3 +11,33 @@ def next_elem_in_for(sequence, position):
         return sequence[int(position)+1]
     except IndexError:
         return None
+    
+
+@register.filter
+def custom_timesince(value):
+    if not isinstance(value, datetime):
+        return value
+
+    # Convert naive datetime to aware datetime
+    if timezone.is_naive(value):
+        value = timezone.make_aware(value, timezone.get_current_timezone())
+
+    now = timezone.now()
+    time_diff = timesince(value, now)
+
+    # Replace 'hours' with 'h' and 'minutes' with 'min'
+    time_diff = time_diff.replace('hour', 'h')
+    time_diff = time_diff.replace('minutes', 'min').replace('minute', 'min')
+    time_diff = time_diff.replace('days', 'd').replace('day', 'd')
+
+    return time_diff
+
+
+
+@register.filter
+def custom_timesince_2(value):
+    # Replace 'hours' with 'h' and 'minutes' with 'min'
+    value = value.replace('hours', 'h').replace('hour', 'h')
+    value = value.replace('minutes', 'min').replace('minute', 'min')
+    value = value.replace('days', 'd').replace('day', 'd')
+    return value
